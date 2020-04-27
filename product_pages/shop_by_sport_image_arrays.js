d56c148df777cff868c8788aa6f39e8b3ebdfed9
garenk95 <garen.kantarci1@gmail.com>
2020-04-27 09:40:31 -0700

    pruned, reorganized images folder...changed shop by sport links to go to same page

---
+++ product_pages/shop_by_sport_image_arrays.js
@@ -0,0 +1,1 @@
+const nba_array = []
---
+++ store.js
@@ -0,0 +1,15 @@
+function load_NBA() {
+    window.location.href = `product_pages/shop_by_sport.html?league=nba`;
+}
+function load_NFL() {
+    window.location.href = `product_pages/shop_by_sport.html?league=nfl`;
+}
+function load_MLB() {
+    window.location.href = `product_pages/shop_by_sport.html?league=mlb`;
+}
+function load_NHL() {
+    window.location.href = `product_pages/shop_by_sport.html?league=nhl`;
+}
+function load_SOCCER() {
+    window.location.href = `product_pages/shop_by_sport.html?league=soccer`;
+}
