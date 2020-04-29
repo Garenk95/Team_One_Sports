6066af2e44bfb987f69d51c840207993fbb03111
Marc Gomes <marcgomes00@gmail.com>
2020-04-29 12:16:54 -0700

    team logo folders created in images

--- product_pages/shop_by_sport_image_arrays.js
+++ product_pages/shop_by_sport_image_arrays.js
@@ -1,1 +1,27 @@
+
+//possibly create 2 arrays: one for the element id, and one the other to populate the id with team logos. 
 const nba_array = []
+
+//NFL AFC for loop
+const nflAfc = ["images/team_logos_NFL/AFC_Teams/bills.png", 
+"images/team_logos_NFL/AFC_Teams/broncos.png","images/team_logos_NFL/AFC_Teams/browns.png",
+"images/team_logos_NFL/AFC_Teams/Chargers.png","images/team_logos_NFL/AFC_Teams/chiefs.png", 
+"images/team_logos_NFL/AFC_Teams/clevelandBrowns.png", "images/team_logos_NFL/AFC_Teams/colts.png",
+"images/team_logos_NFL/AFC_Teams/dolphins.png","images/team_logos_NFL/AFC_Teams/jaguars.png",
+"images/team_logos_NFL/AFC_Teams/jets.png","images/team_logos_NFL/AFC_Teams/patriots.png", 
+"images/team_logos_NFL/AFC_Teams/raiders.png","images/team_logos_NFL/AFC_Teams/ravens.png",
+"images/team_logos_NFL/AFC_Teams/steelers.png","images/team_logos_NFL/AFC_Teams/texans.png",
+"images/team_logos_NFL/AFC_Teams/titans.png" ]
+
+const arrayImagesElement = document.getElementById()
+
+//NFL NFC for loop
+const nflNfc = ["images/team_logos_NFL/NFC_Teams/arizona-cardinals.png", 
+"images/team_logos_NFL/NFC_Teams/bears.gif","images/team_logos_NFL/NFC_Teams/buccaneers.png",
+"images/team_logos_NFL/NFC_Teams/cowboys.png","images/team_logos_NFL/NFC_Teams/eagles.png", 
+"images/team_logos_NFL/NFC_Teams/falcons.png", "images/team_logos_NFL/NFC_Teams/giants.png",
+"images/team_logos_NFL/NFC_Teams/lions.png","images/team_logos_NFL/NFC_Teams/packers.png",
+"images/team_logos_NFL/NFC_Teams/panthers.png","images/team_logos_NFL/NFC_Teams/rams.png", 
+"images/team_logos_NFL/NFC_Teams/redskins.png","images/team_logos_NFL/NFC_Teams/saints.svg",
+"images/team_logos_NFL/NFC_Teams/seahawks.png","images/team_logos_NFL/NFC_Teams/sf 49ers.png",
+"images/team_logos_NFL/NFC_Teams/vikings.png" ]
