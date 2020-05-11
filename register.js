74329c62e248c0e7e933dee7eb1a51c286e15dd6
garenk95 <garen.kantarci1@gmail.com>
2020-05-11 16:50:32 -0700

    register page now saves user data in local storage

---
+++ register.js
@@ -0,0 +1,33 @@
+function test() {
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+    confirm_pass = document.getElementById("confirm_password").value;
+    email = document.getElementById("email").value;
+    phone = document.getElementById("phone_number").value;
+
+    //make sure passwords match
+    passwordsMatch = validatePasswords(password, confirm_pass);
+    if (passwordsMatch == true) {
+        console.log("passwords match");
+        let userData = { username: username, password: password, email: email, phone: phone };
+
+        localStorage.setItem(email, JSON.stringify(userData));
+        console.log(userData);
+        window.location.href = `homepage.html`;
+    } else {
+        //clear password fields
+        //instruct user to re-enter passwords
+        console.log("passwords do not match");
+    }
+
+
+
+}
+
+function validatePasswords(password_1, password_2) {
+    if (password_1 == password_2) {
+        return true;
+    } else {
+        return false;
+    }
+}
